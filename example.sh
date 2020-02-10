169e659913e2f1c8fb55d917e030ea727cd1cccf
Arseny Pavlenko <arsenyp@wix.com>
2020-02-10 17:04:19 +0200

    [+] bash example

---
+++ example.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+echo bash > debug.txt
+
+INPUT=$(cat)
+MODE=$1
+ID=$(echo -n $INPUT | jq .ID)
+PAYLOAD=$(echo $INPUT | jq .Payload)
+# remove wrapping quotes
+if [ ! -z "$ID" ]
+then
+    ID=${ID:1:${#ID} - 2}
+fi
+
+echo "MODE: '$MODE'" >> debug.txt
+echo "STDIN: '$INPUT'" >> debug.txt
+echo "ID: '$ID'" >> debug.txt
+echo "PAYLOAD: '$PAYLOAD'" >> debug.txt
+
+read_resource() {
+    echo "read_resource" >> debug.txt
+    cat tmp/$ID.json
+}
+
+create_resource() {
+    echo "create_resource" >> debug.txt
+    ID=$(date +"%s")
+    echo "ID: '$ID'" >> debug.txt
+    JSON1=$(jq -n "{ID: \"$ID\"}")
+    JSON2=$(jq -n "$PAYLOAD | fromjson")
+    echo "$JSON1 $JSON2" | jq -s add > tmp/$ID.json
+    read_resource
+}
+
+update_resource() {
+    echo "update_resource" >> debug.txt
+    echo "ID: '$ID'" >> debug.txt
+    JSON1=$(jq -n "{ID: \"$ID\"}")
+    JSON2=$(jq -n "$PAYLOAD | fromjson")
+    echo "$JSON1 $JSON2" | jq -s add > tmp/$ID.json
+    read_resource
+}
+
+delete_resource() {
+    echo "update_resource" >> debug.txt
+    echo "ID: '$ID'" >> debug.txt
+    rm tmp/$ID.json
+    echo '{}'
+}
+
+case $MODE in
+    read)
+        read_resource
+        ;;
+    create)
+        create_resource
+        ;;
+    update)
+        update_resource
+        ;;
+    delete)
+        delete_resource
+        ;;
+    *)
+        echo "Unknown mode '$MODE'" >> debug.txt
+        ;;
+
+esac
+
+
+echo "finish" >> debug.txt
--- example.tf
+++ example.tf
@@ -1,17 +1,33 @@
 provider "multiverse" {}
 
-resource "multiverse_custom_resource" "test1" {
+resource "multiverse_custom_resource" "test_node" {
     executor = "node"
     script = "example.js"
     id_key = "ID"
     data = <<JSON
 {
-    "foo": "bar2",
+    "foo": "bar0",
     "bar": "baz"
 }
 JSON
 }
 
-output "test" {
-    value = multiverse_custom_resource.test1
+resource "multiverse_custom_resource" "test_bash" {
+    executor = "bash"
+    script = "example.sh"
+    id_key = "ID"
+    data = <<JSON
+{
+    "foo": "bar11",
+    "bar": "baz"
+}
+JSON
+}
+
+output "test_node" {
+    value = multiverse_custom_resource.test_node
+}
+
+output "test_bash" {
+    value = multiverse_custom_resource.test_node
 }
